5d624ba8ffe32c3a65761a708ec58250ee363bb9
MR MOH MAYA <124359102+THEMOHMAYA@users.noreply.github.com>
2024-08-14 22:18:34 +0530

    Update stream.py

--- AarohiX/utils/stream/stream.py
+++ AarohiX/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from AnonXMusic import Carbon, YouTube, app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.misc import db
-from AnonXMusic.utils.database import add_active_video_chat, is_active_chat
-from AnonXMusic.utils.exceptions import AssistantErr
-from AnonXMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AnonXMusic.utils.pastebin import AnonyBin
-from AnonXMusic.utils.stream.queue import put_queue, put_queue_index
-from AnonXMusic.utils.thumbnails import get_thumb
+from AarohiX import Carbon, YouTube, app
+from AarohiX.core.call import Anony
+from AarohiX.misc import db
+from AarohiX.utils.database import add_active_video_chat, is_active_chat
+from AarohiX import AssistantErr
+from AarohiX.utils.inline import aq_markup, close_markup, stream_markup
+from AarohiX.utils.pastebin import AnonyBin
+from AarohiX.utils.stream.queue import put_queue, put_queue_index
+from AarohiX.utils.thumbnails import get_thumb
 
 
 async def stream(
